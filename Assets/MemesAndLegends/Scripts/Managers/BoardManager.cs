d802b16448d2cd82edf151acf6b3794ce2141b00
Pheonix <leonhumza@gmail.com>
2022-09-24 03:25:02 +0500

    Added Board Manager

--- Assets/MemesAndLegends/Scripts/Managers/BoardManager.cs
+++ Assets/MemesAndLegends/Scripts/Managers/BoardManager.cs
@@ -1,6 +1,7 @@
 using Mono.Cecil.Cil;
 using Photon.Pun;
 using Photon.Realtime;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,6 +13,17 @@ public class BoardManager : MonoBehaviour
     public PhotonView pv;
     public GameObject LoadingUI;
 
+    [Header("Board Rows")]
+    public List<GameObject> R1;
+    public List<GameObject> R2;
+    public List<GameObject> R3;
+    public List<GameObject> R4;
+    public List<GameObject> R5;
+    public List<GameObject> R6;
+    public List<GameObject> R7;
+    public List<GameObject> R8;
+
+    [SerializeField]public List<List<GameObject>> Board;
     private void Awake()
     {
         LoadingUI.SetActive(true);
@@ -23,7 +35,20 @@ public class BoardManager : MonoBehaviour
         
         yield return new WaitForSeconds(3);
         LoadingUI.SetActive(false);
+
+        PopulateBoard();
     
+    }
+    private void PopulateBoard()
+    {
+        Board.Add(R1);
+        Board.Add(R2);
+        Board.Add(R3);
+        Board.Add(R4);
+        Board.Add(R5);
+        Board.Add(R6);
+        Board.Add(R7);
+        Board.Add(R8);
     }
 
 }
